fd7d7d0004ed4617bbabff47d331f65497ca6777
Sourisooo <s.doan@skema.edu>
2023-03-23 19:02:31 +0100

    first commit

--- RingMaster.js
+++ RingMaster.js
@@ -95,10 +95,11 @@ window.addEventListener('load',function() {
             context.fillText('Actually using: '+this.game.input.keys,20,40);
             context.fillText('Combo success: '+this.game.player.combo.success+" //Defense trigger: "+this.game.player.combo.def.trigger,25,150);
             context.fillText('Last combos successfully done: '+this.game.player.combo.combN.join(),25,190);
-            context.fillText('Damage done '+(this.game.player.combo.combD),25,230);
+
+            context.fillText('Damage done '+(this.game.player.combo.combD)+' //Enemy damage blocked: '+this.game.enemy.damageblocked,25,230);
             context.fillText('Defense command: '+(this.game.player.combo.def.key),25,270);
             context.fillText('Combo command: '+(this.game.player.combo.comboA.key),25,310);
-            context.fillText('Actual Combo Performed: '+(this.game.player.combo.actualCombo),25,350);
+            context.fillText('Actual command Performed: '+(this.game.player.combo.actualCombo),25,350);
 
             context.fillText('Ennemy is preparing: '+this.game.enemy.attack.name,1200,800);
             context.fillText('Timer before execution: '+(this.game.enemy.time).toFixed(1)+" of "+this.game.enemy.attack.Cooldown/10,1200,850);
@@ -112,7 +113,7 @@ window.addEventListener('load',function() {
             {context.fillRect(20+5*i,50,3,20)}
 
             const formattedTime = (this.game.gameTime*0.001).toFixed(1);
-            context.fillText('Timer: '+""+(this.game.gameTime*0.001),20,100);
+            context.fillText('Timer before command update: '+""+(this.game.player.combo.time).toFixed(1)+" of "+this.game.player.combo.comboA.timecommand/10,20,100);
 
             if(this.game.gameOver)
 
@@ -154,24 +155,10 @@ window.addEventListener('load',function() {
             this.height=302;
             this.x=0;
             this.y=window.innerHeight/2;
-            // this.speedY=2;
-            // this.speedX=2;
-            // this.maxSpeed=10;
-            // this.projectiles=[];
+
             this.image=player;
-            // this.frameX=0;
-            // this.frameY=0;
-            // this.powerUp=false;
-            // this.powerUpTimer=0;
-            // this.powerUpLimit=0;
-            // this.maxFrame=5;
-            this.shot=[];
             this.combo=new Combo(this);
 
-        //     this.x=this.gameWidth/2-this.width/2
-        // this.y=this.gameHeight-this.height;
-        this.vy=0;
-        this.weight=1;
         this.frameX=0;
         this.frameY=0;
         this.speed=1;
@@ -180,7 +167,7 @@ window.addEventListener('load',function() {
         this.fps=30;
         this.frameTimer=1;
         this.frameInterval=1000/this.fps;
-           
+        
           
 
         }
@@ -198,10 +185,7 @@ window.addEventListener('load',function() {
 
         draw(context,deltaTime){
 
-            // context.fillStyle='black';
-            // context.fillRect(this.x,this.y,this.width,this.height);
 
-            // console.log(deltaTime);
 
             if(this.frameTimer>this.frameInterval)
 
@@ -238,14 +222,20 @@ window.addEventListener('load',function() {
             this.fps =20;
             this.frameInterval =1000/this.fps;
             this.frameTimer=1;
-            this.markedForDeletion=false;
-
+     
             this.game=game;
+          
             this.width=229;
             this.height=171;
-            // this.x=this.game.width;
-            // this.y=this.groundMargin+100;
+
             this.image=enemyrun;
+
+            this.bite=bite;
+            this.firaga=firaga;
+            this.wildrush=wildrush;
+            this.gethit=gethit;
+            this.evade=evade;
+
             this.speedX=Math.random()+30;
             this.speedY=0;
             this.maxFrame=4;
@@ -253,16 +243,23 @@ window.addEventListener('load',function() {
 
             this.x=this.game.width-300;
             this.y=this.game.height/2+50;
+
+            this.enemyanimation=[];
+            this.gethitanimation=[];
+            this.shieldanimaation=[];
+
     
-            this.attackA={Damage:150,Cooldown:2000,name:"bite"};
-            this.attackB={Damage:300,Cooldown:4000,name:"firaga"};
-            this.attackC={Damage:450,Cooldown:6000,name:"wildrush"};
+            this.attackA={Damage:75,Cooldown:2000,name:"bite",animation:new SkillAnimation(this.game,bite,1200,500,256,256,1,0,0,0,0)};
+            this.attackB={Damage:150,Cooldown:4000,name:"firaga",animation:new SkillAnimation(this.game,firaga,1200,500,256,256,1,0,0,0,0)};
+            this.attackC={Damage:300,Cooldown:6000,name:"wildrush",animation:new SkillAnimation(this.game,wildrush,1200,500,256,256,1,0,0,0,0)};
 
             this.attackrandomizer=[this.attackA,this.attackB,this.attackC];
             this.attack={};
             this.damagedone=0;
             this.time=0;
             this.timer=5000;
+            this.key=0;
+            this.damageblocked=0;
        
 
         }
@@ -280,16 +277,39 @@ window.addEventListener('load',function() {
 
        else {
 
+            this.key=Math.floor(Math.random()*3)
 
+           this.attack=this.attackrandomizer[this.key];
 
-           this.attack=this.attackrandomizer[Math.floor(Math.random()*3)];
+           this.enemyanimation.push(new SkillAnimation(this.game,document.getElementById(this.key),1200,500,256,256,1,0,0,0,0));
 
            this.timer=this.attack.Cooldown;
 
-           if((this.game.player.combo.def.trigger==false)){this.damagedone+=this.attack.Damage}
+           if((this.game.player.combo.def.trigger==false))
+            
+                    {
+                        
+                        this.damagedone+=this.attack.Damage;
+                        this.gethitanimation.push(new SkillAnimation(this.game,this.gethit,0,window.innerHeight/2,308,296,1,0,0,0,10));
+                        
+                        this.shieldanimaation=0;
+                        this.shieldanimaation=[];
+
+                        game.player.combo.success=false;
+
+            
+                    
+                    } else {
+                        
+                        this.shieldanimaation.push(new SkillAnimation(this.game,this.evade,400,500,256,256,1,0,0,0,0));
+                        this.damageblocked=this.damageblocked+this.attack.Damage;
+                    
+                         }
        
 
-            console.log(this.attack,this.damagedone,this.timer);
+            // console.log(this.attack,this.damagedone,this.timer);
+
+            // console.log(this.enemyanimation,document.getElementById(this.key));
 
             this.time=0;
 
@@ -313,6 +333,51 @@ window.addEventListener('load',function() {
             
             context.drawImage(this.image,this.frameX*this.width,this.height*this.frameY,this.width,this.height,this.x,this.y,this.width,this.height);
 
+
+       
+
+            this.enemyanimation.forEach(e=>
+                
+
+                {
+
+                    e.draw(context,deltaTime);
+
+
+
+                });
+
+        
+
+               
+      
+                this.gethitanimation.forEach(g=>
+                
+
+                    {
+    
+                        g.draw(context,deltaTime);
+    
+    
+    
+                    });
+
+             
+                    this.shieldanimaation.forEach(a=>
+                
+
+                        {
+        
+                            a.draw(context,deltaTime);
+        
+        
+        
+                        });
+    
+
+                        // console.log(this.gethitanimation);
+
+
         }
 
 
@@ -333,12 +398,17 @@ window.addEventListener('load',function() {
             this.markedForDeletion=false;
             this.image=fireshot;
             this.image2=roll;
+            this.image3=fire;
+            this.cBimage=attack;
+            this.cCimage=spell;
+            this.cDimage=item;
+
             this.defense=[0,1,2,3,4,5,6,7,8,9]
             this.def = {key:[],name:"defense",trigger:false};
-            this.comboA = {key:[],name:"",damage:0,index:0,timecommand:5000};
-            this.comboB = {key:["A","B","C","D"],name:"attack",damage:50,index:1,timecommand:2000};
-            this.comboC = {key:["X","ArrowUp","W","M","ArrowRight"],name:"spell",damage:200,index:2,timecommand:3000};
-            this.comboD = {key:["A","ArrowUp","ArrowDown","M","ArrowRight"],name:"item",damage:300,index:3,timecommand:3500};
+            this.comboA = {key:[],name:"",damage:0,index:0,timecommand:0};
+            this.comboB = {key:["A","B","C","D"],name:"attack",damage:50,index:1,timecommand:3000,icon:attack};
+            this.comboC = {key:["X","ArrowUp","W","M","ArrowRight"],name:"spell",damage:200,index:2,timecommand:4500,icon:spell};
+            this.comboD = {key:["A","ArrowUp","ArrowDown","M","ArrowRight"],name:"item",damage:300,index:3,timecommand:5000,icon:item};
             this.actualCombo = [];
             this.success=false;
             this.combN=[];
@@ -346,7 +416,7 @@ window.addEventListener('load',function() {
             this.timer=5000;
             this.comboRamdomize = [this.comboB,this.comboC,this.comboD ];
             this.interval=1;
-            this.time=0;
+            this.time=500;
 
             this.x=0;
             this.y=window.innerHeight/2;
@@ -393,18 +463,20 @@ window.addEventListener('load',function() {
              this.time=0}
 
 
-            if(this.comboA.key.join() == this.actualCombo.join())
+            if((this.comboA.key.join() == this.actualCombo.join())&&(this.comboA.key.join()!=""))
             
-            {this.combN.push(this.comboA.name),this.combD=this.combD+this.comboA.damage,this.success=true;game.gameTime=0,this.actualCombo=0;this.actualCombo=[],this.time=500}
+            {this.combN.push(this.comboA.name),this.combD=this.combD+this.comboA.damage,this.success=true;game.gameTime=0,this.actualCombo=0,this.actualCombo=[],this.time=this.timer/10,game.enemy.gethitanimation=0,game.enemy.gethitanimation=[]}
 
             else if(this.comboA.key.length==this.actualCombo.length){this.actualCombo=0;this.actualCombo=[];this.success=false};
             
             // if(this.comboA.timecommand>game.gameTime){this.success=false};
           
 
-            if(this.def.key.join() == this.actualCombo.join())
+            if((this.def.key.join() == this.actualCombo.join())&&(this.def.key.join()!=""))
             
-            {this.def.trigger=true;}
+            {this.def.trigger=true;game.enemy.gethitanimation=0;game.enemy.gethitanimation=[]}
+
+            // console.log(game.enemy.gethitanimation);
 
 
             if(this.success==true){
@@ -417,7 +489,7 @@ window.addEventListener('load',function() {
                 
                 this.frameX=0;
             
-                {this.projectiles.push(new SkillAnimation(this.game,0,400));};
+                {this.projectiles.push(new SkillAnimation(this.game,this.image3,200,610,209,54,20,10,0,0,14));};
 
                 // console.log(this.x,this.y,this.projectiles);
             }
@@ -506,29 +578,30 @@ window.addEventListener('load',function() {
 
     class SkillAnimation {
 
-        constructor(game){
+        constructor(game,animation,x,y,width,height,speed,speedC,frameX,frameY,maxFrame){
 
                 this.game=game;
 
-                this.width=209;
-                this.height=54;
+                this.width=width;
+                this.height=height;
     
-                this.speed=200;
+                this.speed=speed;
+                this.speedC=speedC;
                 this.markedForDeletion=false;
-          
-                this.image=fire;
-
-                this.timer=5000;
+        
+                this.png=animation;
+                this.timer=3000;
                 this.interval=1;
                 this.time=0;
-    
-                this.x=275;
-                this.y=window.innerHeight/2+125;
-                this.frameX=0;
-                this.frameY=0;
+
+                this.x=x;
+                this.y=y;
+
+                this.frameX=frameX;
+                this.frameY=frameY;
                 this.speed=1;
                 this.maxSpeed=4;
-                this.maxFrame=16;
+                this.maxFrame=maxFrame;
                 this.fps=30;
                 this.frameTimer=1;
                 this.frameInterval=1000/this.fps;
@@ -543,7 +616,7 @@ window.addEventListener('load',function() {
 
         update(deltaTime){
 
-            this.x+=this.speed*10;
+            this.x+=this.speed*this.speedC;
             // console.log(this.x,this.speed);
 
 
@@ -553,9 +626,25 @@ window.addEventListener('load',function() {
         draw(context,deltaTime){
 
 
-            context.drawImage(this.image,0,0,this.width,this.height, this.x,this.y,this.width,this.height);
 
 
+
+
+        if(this.frameTimer>this.frameInterval)
+
+            {
+
+                if(this.frameX<this.maxFrame)this.frameX++;
+                else this.frameX=0;
+                this.frameTimer=0;
+            } else {this.frameTimer+=deltaTime;};
+
+
+
+            context.drawImage(this.png,this.width*this.frameX,this.height*this.frameY,this.width,this.height, this.x,this.y,this.width,this.height);
+
+
+                
         }
 
     
@@ -587,7 +676,8 @@ window.addEventListener('load',function() {
            this.gameTime+=deltaTime;
 
             this.player.update(deltaTime);
-         
+              
+            this.enemy.update(deltaTime);
          
             };
 
@@ -601,8 +691,6 @@ window.addEventListener('load',function() {
 
             this.ui.draw(context);
             this.player.draw(context,deltaTime);
-        
-            this.enemy.update();
             this.enemy.draw(context,deltaTime);
 
             // console.log(this.enemy);
